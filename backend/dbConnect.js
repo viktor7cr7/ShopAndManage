ffbefb8a494edad4899493972f7f50334a9df0d3
viktor7cr7 <volkovatatiana1919@gmail.com>
2024-07-26 00:53:00 +0300

    rename correct dbadmin

--- backend/dbConnect.js
+++ backend/dbConnect.js
@@ -2,10 +2,10 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import * as pgp from 'pg-promise';
 
-console.log('PGP_URL:', process.env.PGP_URL);
+/* console.log('PGP_URL:', process.env.PGP_URL); */
 console.log('PGP_URL_ADMIN:', process.env.PGP_URL_ADMIN);
 
-const dbConnect = pgp.default()(process.env.PGP_URL);
+/* const dbConnect = pgp.default()(process.env.PGP_URL); */
 const dbConnectAdmin = pgp.default()(process.env.PGP_URL_ADMIN);
 
 export { dbConnect, dbConnectAdmin };
